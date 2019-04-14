60c007609c2b58326999ed95c2ef2e8e4483189f
acl <acl447@nyu.edu>
2019-04-14 15:42:56 -0700

    refactored code

--- gifTastic.js
+++ gifTastic.js
@@ -1,10 +1,12 @@
 
+//Array of sport names to put on buttons
+
 var topics = ["volleyball", "basketball", "swimming", "soccer", "golf", "surfing", "football", "baseball",
     "softball", "lacrosse", "field hockey", "tennis", "hockey", "rowing", "bowling", "diving", "paddleboarding", "skiing",
     "gymnastics", "skateboarding", "snowboarding"];
 
 
-
+//For every sport name in the array, create a button and append it to the gif button section
 
 for (var i = 0; i < topics.length; i++) {
 
@@ -22,26 +24,29 @@ for (var i = 0; i < topics.length; i++) {
 
 
 
-
+//When the submit button is clicked
 $(document.body).on("click", "#submit", function () {
 
+    //Grab the name of the sport that user entered in textbox
     var inputValue = $("#input-box").val();
 
     var newButton = $("<button>");
 
+    //Create a new button with that sport name as its text
     newButton.text(inputValue);
 
     newButton.attr("class", "btn btn-info btn-sm mx-2 my-1");
 
     newButton.attr("id", "gif-btn");
 
+    //Append the new button to the gif button section
     $("#gif-btn-section").append(newButton);
 
 
 });
 
 
-
+//When a gif button is clicked
 $(document.body).on("click", "#gif-btn", function () {
 
     var buttonText = $(this).text();
@@ -51,7 +56,7 @@ $(document.body).on("click", "#gif-btn", function () {
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=dTbudSbvuU8M5Az7O0DnfqU6CIYbmLT8&limit=10";
 
 
-
+    //Make an AJAX call to the GIPHY API
     $.ajax({
 
         url: queryURL,
@@ -62,12 +67,15 @@ $(document.body).on("click", "#gif-btn", function () {
 
     }).then(function (response) {
 
+        
         var gifArray = response.data;
 
+        //Loop through the array of response data from the GIPHY API
         for (var i = 0; i < gifArray.length; i++) {
 
             var gifDiv = $("<div id='gif-div'>");
 
+            //Grab gif rating, still image, and animated image data
             var gifRating = gifArray[i].rating;
 
             var imgURL = gifArray[i].images.fixed_height_still.url;
@@ -84,6 +92,7 @@ $(document.body).on("click", "#gif-btn", function () {
 
             gifImageElement.attr("data-state", "still");
 
+            //Append data to gif section
             gifDiv.append(gifImageElement);
 
             gifDiv.append("Rating: " + gifRating);
